10aa60838ef1df0886e75e9abcf683592f1ee0b9
Valin Short <valin.short1998@gmail.com>
2018-01-26 15:06:03 -0500

    loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+   int count = 0;
+   // while loops
+
+        boolean whileCondition = true;
+        while(whileCondition){
+            System.out.println(" Our while condition is true. Adding one to our count");
+            count = count + 1;
+            if (count == 3){
+            System.out.println("Our count is at 3");
+            whileCondition = false;
+        }
+
+   
+
+
+
+
+
+
     }
+
+
+
+
+
 }
